906a479966832024b05e33c7bbf32276417f567c
阳光喷嚏人 <103102106+YuhangYe0821@users.noreply.github.com>
2022-06-26 17:01:19 +0800

    2022.6.26
    添加了挂售和求购的美刀价格（1rmb=0.1493$）

--- steam.java
+++ steam.java
@@ -11,20 +11,22 @@ public class steam {
         double result4;//求购到手
         Scanner input=new Scanner(System.in);
         while(true){//死循环
-            System.out.println("买入:");//buy
-            num1=input.nextDouble();//输入购入价
-            System.out.println("卖出(挂售):");//sell
-            num2=input.nextDouble();//输入steam售价
-            System.out.println("卖出(求购):");//sell
-            num3=input.nextDouble();//输入steam售价
+            System.out.print("买入(￥):");//buy
+            num1=input.nextDouble();//输入 rmb购入价
+            System.out.print("挂售(￥):");//sell
+            num2=input.nextDouble();//输入 rmb挂售价
+            System.out.print("求购(￥):");//sell
+            num3=input.nextDouble();//输入 rmb求购价
             result1=num1/(num2/1.15);//挂售比例
             result2=num1/(num3/1.15);//挂售比例
             result3=num2/1.15;//挂售到手
             result4=num3/1.15;//求购到手
-            System.out.println("挂售比例："+result1);//输出比例proportion
-            System.out.println("求购比例："+result2);//输出比例proportion
-            System.out.println("挂售到手："+result3);//输出到手Get
-            System.out.println("求购到手："+result4);//输出到手Get
+            System.out.println("挂售($)："+num2*0.1493);//输出 美刀挂售价
+            System.out.println("求购($)："+num3*0.1493);//输出 美刀求购价
+            System.out.println("挂售比例："+result1);//输出 挂售比例proportion
+            System.out.println("求购比例："+result2);//输出 求购比例proportion
+            System.out.println("挂售到手(￥)："+result3);//输出 挂售到手Get
+            System.out.println("求购到手(￥)："+result4);//输出 求购到手Get
             System.out.println();//换行
         }
     }
